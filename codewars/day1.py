ff40d19dacb043856d70b8566312f8b8abb94c0b
MrSushi201 <santi@taweesamarn.com>
2023-11-24 11:31:24 +0100

    # FIX day1.py according to problem description

--- codewars/day1.py
+++ codewars/day1.py
@@ -1,7 +1,9 @@
 # Complete the square sum function so that it squares each number passed into it and then sums the results together.
+# Example: for [1, 2, 2], it should return 9 because 1^2 + 2^2 + 2^2 = 9
+
 # Define function
 def square_sum(nums):
     sq_num = sum((_ * _) for _ in nums)
     return sq_num, print(f"Sum of square(nums): {sq_num}")
 
-square_sum([1, 2, 3])
+square_sum([1, 2, 2])
