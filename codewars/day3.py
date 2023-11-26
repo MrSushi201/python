77850d60f9ae84e051807fbc2331902687c7a8f4
MrSushi201 <santi@taweesamarn.com>
2023-11-26 16:21:50 +0100

    # UPDATE day3.py with another Kata - Invert values

--- codewars/day3.py
+++ codewars/day3.py
@@ -15,3 +15,16 @@ def simple_multiplication(number):
         return res
     
 simple_multiplication(8)
+
+# Invert values
+# Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+# invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+# invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+# invert([]) == []
+# You can assume that all values are integers. Do not mutate the input array/list.
+
+# Define function
+def invert(lst):
+    new_lst = [-x for x in lst]
+    print(f"Inverse of given list: {new_lst}")
+    return new_lst
