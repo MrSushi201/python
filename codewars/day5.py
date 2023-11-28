576633bb365016982ace3009da8ad31acb9a546f
MrSushi201 <santi@taweesamarn.com>
2023-11-28 11:23:42 +0100

    # UPDATE day5.py with another kata

--- codewars/day5.py
+++ codewars/day5.py
@@ -6,3 +6,16 @@ def make_upper_case(s: str):
     res = s.upper()
     print(res)
     return res
+
+# String Repeat
+# Write a function that accepts an integer n, and a string s as parameters.
+# Return a string of s repeated exactly n times. 
+# Examples: Input --> Output
+#           6, "I" --> "IIIIII"
+#           5, "Hello" --> "HelloHelloHelloHelloHello"¨
+
+# Define function
+def repeat_str(n: int, s: str):
+    res = s * n
+    print(res)
+    return res
