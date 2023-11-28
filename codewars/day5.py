4066504448a245bbf9169e6fd63d0bf28ff8190d
MrSushi201 <santi@taweesamarn.com>
2023-11-28 11:20:37 +0100

    # UPDATE day5.py with daily kata

---
+++ codewars/day5.py
@@ -0,0 +1,8 @@
+# MakeUpperCase
+# Write a function which converts the input string to uppercase.
+
+# Define function
+def make_upper_case(s: str):
+    res = s.upper()
+    print(res)
+    return res
