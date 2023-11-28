f84b2dd980c6acdec339d0839e5c5557a7c28d2c
MrSushi201 <santi@taweesamarn.com>
2023-11-28 12:17:03 +0100

    # ADD holypython and lesson1_print

---
+++ holypython/lesson1_print.py
@@ -0,0 +1,35 @@
+"""
+Lesson 1: print()
+The print()-function is used to display data and information to the user. 
+It can be used to check:
+    values, 
+    variables, 
+    make reminders, or 
+    show messages 
+to the user or the programmer at different stages of the coding process. 
+"""
+
+# The most common programming ritual in all programming languages
+print("Hello, World!")
+
+# Advanced concepts - printing with commas
+print("Value1", "Value2", "Value3")
+
+# Advanced concepts - using "sep" and "end" parameters
+print("Hello, world!", sep = " ", end = "\n") # default
+
+# Exercise 1-a: print "Hello, World!"
+print("Hello, World!")
+
+# Exercise 1-b: Assign "Hello, World!" to a variable, and print it
+str = "Hello, World!"
+print(str)
+
+# Exercise 1-c: Concatenate different data pieces with print()-function and commas
+print("Value1", "Value2", "Value3")
+var1 = "My"
+var2 = "name"
+var3 = "is"
+var4 = "Santi"
+var5 = "."
+print(var1, var2, var3, var4, var5)
