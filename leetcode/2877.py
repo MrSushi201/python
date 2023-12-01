78084242dfb3fa146efa0e3b6abc25f7eb369e5f
MrSushi201 <santi@taweesamarn.com>
2023-12-01 20:57:48 +0100

    # ADD 2877.py - Create a DataFrame from list

---
+++ leetcode/2877.py
@@ -0,0 +1,11 @@
+# Create a DataFrame from List
+# Write a solution to create a DataFrame from a 2D list called student_data.
+# This 2D list contains the IDs and ages of some students.
+# The DataFrame should have two columns, student_id and age, and be in the same order as the original 2D list.
+import pandas as pd
+
+# Define function
+def createDataframe(student_data: list[list[int]]) -> pd.DataFrame:
+    columns = ["student_id", "age"]
+    df = pd.DataFrame(student_data, columns = columns)
+    return df
