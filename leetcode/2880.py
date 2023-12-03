e99be3c17db7a47f87cd16dbebd51085d7fd3c6d
MrSushi201 <santi@taweesamarn.com>
2023-12-03 17:13:47 +0100

    # UPDATE 2880.py with an alternative solution

--- leetcode/2880.py
+++ leetcode/2880.py
@@ -6,5 +6,10 @@
 import pandas as pd
 
 def selectData(students: pd.DataFrame) -> pd.DataFrame:
-    data = students.loc[students["student_id"] == 101, ["name", "age"] ]
+    # Solution 1
+    # data = students.loc[students["student_id"] == 101, ["name", "age"]]
+
+    # Solution 2
+    data = students[students["student_id"] == 101][["name", "age"]]
+
     return data
