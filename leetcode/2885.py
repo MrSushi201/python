2fea8fa0fa5979fe859f8cf1e17b34ab460e1134
MrSushi201 <santi@taweesamarn.com>
2023-12-10 17:12:42 +0100

    # ADD 2885.py

---
+++ leetcode/2885.py
@@ -0,0 +1,31 @@
+# 2885. Rename Columns
+# DataFrame students
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | id          | int    |
+# | first       | object |
+# | last        | object |
+# | age         | int    |
+# +-------------+--------+
+# Write a solution to rename the columns as follows:
+    # id to student_id
+    # first to first_name
+    # last to last_name
+    # age to age_in_years
+# The result format is in the following example.
+
+# Define function / solution
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    students.rename(
+        columns = {
+            "id": "student_id",
+            "first": "first_name",
+            "last": "last_name",
+            "age": "age_in_years",
+        },
+        inplace = True
+    )
+    return students
