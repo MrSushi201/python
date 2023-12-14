e06e32e4a7524f9f31d53807a3caad9b77d5a8be
MrSushi201 <santi@taweesamarn.com>
2023-12-15 00:41:19 +0100

    # ADD 2883_drop_missing_data.py

--- leetcode/2883_drop_missing_data.py
+++ leetcode/2883_drop_missing_data.py
@@ -1,5 +1,5 @@
 # 2883. Drop Missing Data
-# DataFrame: students
+# DataFrame students
 # +-------------+--------+
 # | Column Name | Type   |
 # +-------------+--------+
@@ -8,6 +8,7 @@
 # | age         | int    |
 # +-------------+--------+
 # There are some rows having missing values in the name column.
+
 # Write a solution to remove the rows with missing values.
 # The result format is in the following example.
 
@@ -15,4 +16,5 @@
 import pandas as pd
 
 def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
-    return students.dropna(axis = 0, subset = ["name"])
+    df = students.dropna(axis = 0, subset = ["name"])
+    return df
