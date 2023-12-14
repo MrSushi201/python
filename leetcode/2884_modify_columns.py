1b40d6b2172a45ac23b82d3ea36125425b803b8a
MrSushi201 <santi@taweesamarn.com>
2023-12-15 00:43:25 +0100

    # ADD 2884_modify_columns.py

--- leetcode/2884_modify_columns.py
+++ leetcode/2884_modify_columns.py
@@ -6,8 +6,8 @@
 # | name        | object |
 # | salary      | int    |
 # +-------------+--------+
-
 # A company intends to give its employees a pay rise.
+
 # Write a solution to modify the salary column by multiplying each salary by 2.
 # The result format is in the following example.
 
