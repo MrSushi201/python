5e6c402acd0b2186428526c28e1a79d1f386ce43
MrSushi201 <santi@taweesamarn.com>
2023-12-15 00:48:13 +0100

    # ADD 2887_fill_missing_data.py

--- leetcode/2887_fill_missing_data.py
+++ leetcode/2887_fill_missing_data.py
@@ -7,8 +7,8 @@
 # | quantity    | int    |
 # | price       | int    |
 # +-------------+--------+
+
 # Write a solution to fill in the missing value as 0 in the quantity column.
-# The result format is in the following example.
 
 # Define function/solution
 import pandas as pd
