7669e81dfbee6c5db85d2840eb4ad5aee534e3dc
MrSushi201 <santi@taweesamarn.com>
2023-12-15 00:39:27 +0100

    # ADD 2882_drop_duplicate_rows.py

--- leetcode/pandas/2882_drop_duplicate_rows.py
+++ leetcode/pandas/2882_drop_duplicate_rows.py
@@ -1,5 +1,4 @@
 # 2882. Drop Duplicate Rows
-# DataFrame: customers
 # DataFrame customers
 # +-------------+--------+
 # | Column Name | Type   |
@@ -9,6 +8,7 @@
 # | email       | object |
 # +-------------+--------+
 # There are some duplicate rows in the DataFrame based on the email column.
+
 # Write a solution to remove these duplicate rows and keep only the first occurrence.
 # The result format is in the following example.
 
@@ -16,5 +16,6 @@
 import pandas as pd
 
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
-    return customers.drop_duplicates(subset = ["email"], keep = "first")
+    res_df = customers.drop_duplicates(subset = ["email"], keep = "first")
+    return res_df
     
