604ff5565195caa8a0a35b0f0fc9c5aa4f63e624
MrSushi201 <santi@taweesamarn.com>
2023-12-14 12:49:32 +0100

    # ADD 2878_get_the_size_of_a_dataframe.py

--- leetcode/2878_get_the_size_of_a_dataframe.py
+++ leetcode/2878_get_the_size_of_a_dataframe.py
@@ -1,9 +1,19 @@
 # 2878. Get the Size of a DataFrame
-# DataFrame: players
+# DataFrame players:
+# +-------------+--------+
+# | Column Name | Type   |
+# +-------------+--------+
+# | player_id   | int    |
+# | name        | object |
+# | age         | int    |
+# | position    | object |
+# | ...         | ...    |
+# +-------------+--------+
+
 # Write a solution to calculate and display the number of rows and columns of player.
 # Return the result as an array: [number of rows, number of columns].
 
-# Define solution and function
+# Define function / solution
 import pandas as pd
 
 def getDataframeSize(players: pd.DataFrame) -> list[int]:
